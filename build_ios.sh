2638d01b5b705b3c55fda66256ef70a6ac31fbf6
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2023-01-07 08:19:50 +0700

    Fix iOS build script

--- build_ios.sh
+++ build_ios.sh
@@ -1,1 +1,7 @@
-make -j4 SDK_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
+make -j4 \
+  glslang_path=external/glslang \
+  spirv_cross_1_2_include_path=external/SPIRV-Headers/include/spirv/1.2 \
+  spirv_cross_config_include_path=external/SPIRV-Cross \
+  spirv_cross_lib_path=external/SPIRV-Cross/build \
+  SDK_ROOT=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk
+ldid -S build/libmgl.dylib
